b6e429f6c99dbd496770e8b4c7d54112ee271b10
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-06-12 13:14:24 +0100

    add recipe template added and wired up to the database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -16,6 +17,27 @@ recipes_collection = mongo.db.recipes
 @app.route('/get_recipes')
 def get_recipes():
   return render_template('recipes.html', recipes=recipes_collection.find())
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
+def add_recipe():
+  if request.method =='POST':
+    form = request.form.to_dict()
+    # currentDate = datetime.datetime.now()
+    recipes = recipes_collection
+    recipes.insert_one(
+      {
+        'name': form['name'],
+        'author': form['author'],
+        'cuisine': form['cuisine'],
+        'allergens': form['allergens'],
+        'ingredients': (form['ingredients'].split(',')),
+        'preparation': form['preparation'],
+        'like': bool('false'),
+        'votes': int('0'),
+        'date_added': datetime.datetime.now()
+      }
+    )
+  return render_template('add_recipe.html')
 
 # user registration
 @app.route('/register', methods=['GET', 'POST'])
@@ -40,7 +62,7 @@ def register():
         )
         flash("Your account has been created.", "success")
     else:
-      flash("Passwords do not match", "warning")
+      flash("Passwords do not match")
 
   return render_template('register.html')
 
